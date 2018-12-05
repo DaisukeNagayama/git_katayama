40f431c88549118fa8fbdbf01d08ed7cd1f3b96c
nagayama1729 <39546428+nagayama1729@users.noreply.github.com>
2018-12-05 19:56:30 +0900

    [organize] フォルダの名前変更
    モデルデータの容量が大きくてどう管理するか悩ましい

--- 完全一致検索_forSTL_2/久米フォルダ/resource/GetFileList.m
+++
@@ -1,9 +0,0 @@
-function [fileList, dirList] = GetFileList(dirPath)
-%GetFileList 引数で指定したフォルダ直下にあるファイル名・フォルダ名のリストを取得
-%   詳細説明をここに記述
-  temp = dir(dirPath);
-  temp(1:2) = [];
-  fileList = {temp([temp.isdir] == 0).name}'; % フォルダ以外のリスト
-  dirList = {temp([temp.isdir] == 1).name}';  % フォルダのリスト
-end
-
